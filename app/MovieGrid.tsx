e3c171a92a29a2080684636bf1d45d1389b91b26
beaverDamDemo <zdenevi@gmail.com>
2025-12-01 21:06:59 +0100

    style updates for popover

--- app/MovieGrid.tsx
+++ app/MovieGrid.tsx
@@ -77,7 +77,7 @@ export default function MovieGrid({ movies }: { movies: any[] }) {
 
       {selectedMovie && (
         <div
-          className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
+          className="fixed inset-0 bg-black/80 flex items-center justify-center z-50"
           onClick={() => setSelectedMovie(null)}
         >
           <div
