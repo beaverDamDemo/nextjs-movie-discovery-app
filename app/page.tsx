0417d96990770c9437bb744d881801800dca7b0b
beaverDamDemo <zdenevi@gmail.com>
2025-12-01 17:22:45 +0100

    done some layout tuning for grid

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default async function Page() {
       <main className={styles.main}>
         <div>
           <h1 className="text-2xl font-bold">Popular Movies</h1>
-          <ul className="mt-5 grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+          <ul className="mt-5 grid gap-4 grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8">
             {popularMovies.results.map((movie: any) => (
               <li
                 key={movie.id}
